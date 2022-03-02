4cf7c6743228b1783eb00233da3f3e22dc9266bb
Ansar <78149480+Ansar1337@users.noreply.github.com>
2022-03-02 19:41:32 +0000

    updated user entity

--- src/main/java/hibernate/entity/User.java
+++ src/main/java/hibernate/entity/User.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "user_data", schema = "todolist", catalog = "postgres")
@@ -12,12 +16,9 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Cacheable
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class User {
-
-    public User(Long id, String email) {
-        this.id = id;
-        this.email = email;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,8 +26,29 @@ public class User {
 
     private String email;
 
-    @Column(name = "userpassword",nullable = false)
+    private String username;
+
+    @Column(name = "userpassword", nullable = false)
     private String password;
 
-    private String username;
+    @ManyToMany(mappedBy = "users", fetch = FetchType.EAGER)
+    private Set<Role> roles;
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
 }
