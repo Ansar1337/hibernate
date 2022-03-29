0c100728354d8ff06793b21ca7f61e5b012ee7a1
Ansar <78149480+Ansar1337@users.noreply.github.com>
2022-03-29 20:09:07 +0100

    updating user information and checking its roles

--- src/main/java/hibernate/MainDAO.java
+++ src/main/java/hibernate/MainDAO.java
@@ -24,6 +24,18 @@ public class MainDAO {
         u.setUsername("Michael");
         u.setEmail("michaelbusiness@username.com");
         userDAO.add(u);
+        
+        // Updating the user information
+        User u2 = new User();
+        u2.setId(20036L);
+        u2.setUsername("Jim");
+        u2.setEmail("jim@yahoo.com");
+        u2.setPassword("nopass1");
+        userDAO.update(u2);
+
+        // Checking its role(s)
+        u2 = userDAO.get(20036L);
+        log.info(u2.getRoles());
 
         HibernateUtil.close(); // close Session Factory - clears the 2nd level cache
     }
