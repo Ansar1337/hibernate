a5a9de7ef90df8b60f2baf06310f69b3ae16d191
Ansar <78149480+Ansar1337@users.noreply.github.com>
2022-03-31 19:50:17 +0100

    completing the specific task (Scenario)
    completing the specific scenario alongside using all other DAO methods

--- src/main/java/hibernate/MainDAO.java
+++ src/main/java/hibernate/MainDAO.java
@@ -41,6 +41,63 @@ public class MainDAO {
         userDAO.delete(20036L);
         log.info(userDAO.findAll("@email.com"));
         log.info(userDAO.findAll());
+        
+        /*  SCENARIO (one of the possible options - you can come up with your own):
+        1. create a user (triggers are created immediately test data)
+        2. activate the user (the field is activated)
+        3. create several new tasks (besides tests) with added category and priority
+        complete the table
+        4. delete brushes
+        5. read statistics for any category of users and calculate the overall user statistics
+      */
+
+        // create a user (triggers are created immediately test data)
+        User user = new User();
+        user.setUsername("testuser");
+        user.setPassword("testuser");
+        user.setEmail("testuser@gmail.com");
+
+        UserDAOImpl userDAO2 = new UserDAOImpl();
+        userDAO2.add(user);
+
+        // activate the user (the field is activated)
+        activityDAO = new ActivityDAOImpl();
+        Activity activity = activityDAO.getByUser(user);
+        activity.setActivated(true);
+        activityDAO.update(activity);
+
+        // create several new tasks (besides tests) with added category and priority
+        Priority priority = new Priority();
+        priority.setColor("#fff");
+        priority.setTitle("New Priority");
+        priority.setUser(user);
+        priorityDAO.add(priority);
+
+        Category category = new Category();
+        category.setTitle("New category");
+        category.setUser(user);
+        categoryDAO.add(category);
+
+        Task task = new Task();
+        task.setUser(user);
+        task.setTitle("New Task2");
+        task.setCategory(category);
+        task.setPriority(priority);
+        task.setTaskDate(new Date());
+        task.setCompleted(false);
+        taskDAO.add(task);
+
+        task.setCompleted(true);
+        taskDAO.update(task);
+
+        // deletion
+        taskDAO.delete(task.getId());
+
+        // read statistics for any category of users and calculate the overall user statistics
+        Stat stat = statDAO.getByUser(user);
+
+        log.info(stat.getCompletedTotal());
+        log.info(category.getCompletedCount());
 
         HibernateUtil.close(); // close Session Factory - clears the 2nd level cache
     }
